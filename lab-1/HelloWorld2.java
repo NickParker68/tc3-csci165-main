bb2ca8c83207e9fab6a00394481fd2b0ee827dd6
Nick Parker <nicholas.paker55@gmail.com>
2020-01-30 20:28:52 -0500

    Submission for Lab 1

---
+++ lab-1/HelloWorld2.java
@@ -0,0 +1,11 @@
+// Nick Parker CSCI165
+// 1/28/20
+public class HelloWorld2{
+
+    // main method is the starting point of any Java app
+    public static void main(String[] args){
+
+        System.out.println("Hello " + args[0] + " " + args[1] + ". Nice work processing the arguments");
+
+    } // end of main
+} // end of class
---
+++ lab-1/Ques_10.java
@@ -0,0 +1,17 @@
+// Nick Parker
+// CSCI165 1/30/20
+// Question 10 Lab 1
+
+public class Ques_10{
+
+    //main method
+    public static void main(String[] args){
+
+
+      System.out.println("*****     *****     *****");
+      System.out.println("****      *   *      ***");
+      System.out.println("***       *   *       **");
+      System.out.println("**        *   *      ***");
+      System.out.println("*         *****     *****");
+    } // end of main
+} // end of class
